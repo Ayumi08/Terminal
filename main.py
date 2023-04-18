bb4a00812d88ddaee63b1c91fcc9865bc64eef2b
ayuminakayama265@gmail.com <ayuminakayama265@gmail.com>
2023-04-17 21:47:44 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import colorama
+import os
+import readchar
+
+colorama.init(autoreset=True,)
+
+USERNAME = "jwu"
+
+os.system('cls')
+print(
+    'Microsoft Windows [Version 10.0.19044.2846]\n(c) Microsoft Corporation. All rights reserved.\n\n')
+
+try:
+    os.chdir(f'C:/Users/{USERNAME}')
+except FileNotFoundError:
+    print(colorama.Fore.RED + colorama.Style.BRIGHT +
+          'Critical Error!' + colorama.Fore.WHITE + '\n\nFileNotFoundError: [WinError 2] The system cannot find the path specified: C:/Users/{USERNAME}\n\nAre you sure you typed your USERNAME correctly?')
+
+
+working_dir = os.getcwd()
+
+print(working_dir+'>')
